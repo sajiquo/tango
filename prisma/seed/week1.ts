5c893fa9710c2dbd548bac8722631c075d8f893a
sajiquo <szmckw@gmail.com>
2023-04-04 14:28:48 +0000

    chore: prepare seed

---
+++ prisma/seed/week1.ts
@@ -0,0 +1,17 @@
+import type { LessonInit } from "./seed";
+
+export const week1: LessonInit = {
+  name: "week1",
+  words: [
+    {
+      name: "astute",
+      meaning: "accureate",
+      sentences: [
+        {
+          text: "hoge fuga",
+          inflection: "fuga",
+        },
+      ],
+    },
+  ],
+};
